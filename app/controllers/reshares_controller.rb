d4f9209dd77f984c2052b619d0984e20240a5b2b
danielgrippi <danielgrippi@gmail.com>
2012-06-10 22:26:55 -0700

    remix action lulz

--- app/controllers/reshares_controller.rb
+++ app/controllers/reshares_controller.rb
@@ -17,7 +17,20 @@ class ResharesController < ApplicationController
   end
 
   def fb_create
-    Rails.logger.info("PARAMS FROM FACEBOOK: #{params.inspect}")
-    render :json => {"success" => false}, :format => :json
+    signed_request = decode_data(params[:signed_request])
+    render :json => {:redirect => signed_request["objects"].first["url"] + "/remix"}
+  end
+
+  protected
+
+  def base64_url_decode str
+    encoded_str = str.gsub('-','+').gsub('_','/')
+    encoded_str += '=' while !(encoded_str.size % 4).zero?
+    Base64.decode64(encoded_str)
+  end
+
+  def decode_data str
+    encoded_sig, payload = str.split('.')
+    data = ActiveSupport::JSON.decode base64_url_decode(payload)
   end
 end
