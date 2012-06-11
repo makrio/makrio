7ddb30ee973cda7fdc489b665654150dba5d7489
danielgrippi <danielgrippi@gmail.com>
2012-06-10 23:42:40 -0700

    allow null user_ids in services table to allow for the signup process

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611062522) do
+ActiveRecord::Schema.define(:version => 20120611063925) do
 
   create_table "account_deletions", :force => true do |t|
     t.string  "diaspora_handle"
@@ -351,7 +351,7 @@ ActiveRecord::Schema.define(:version => 20120611062522) do
 
   create_table "services", :force => true do |t|
     t.string   "type",          :limit => 127, :null => false
-    t.integer  "user_id",                      :null => false
+    t.integer  "user_id"
     t.string   "uid",           :limit => 127
     t.string   "access_token"
     t.string   "access_secret"
