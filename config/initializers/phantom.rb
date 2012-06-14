b1f44dffe722e7d69057439ee13e495f7fd0781f
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-14 02:55:13 -0700

    new strat for phantom

---
+++ config/initializers/phantom.rb
@@ -0,0 +1,6 @@
+if RUBY_PLATFORM.downcase.match(/darwin/)
+  PHANTOM_PATH = 'phantomjs'
+else
+  `[ -f #{Rails.root}/vendor/phantomjs  ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz | tar zx)`
+  PHANTOM_PATH = '/app/vendor/phantomjs/bin/phantomjs'
+end
