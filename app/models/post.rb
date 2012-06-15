2e18b3d19994504327f46e8649013c8f521f0d22
danielgrippi <danielgrippi@gmail.com>
2012-06-15 12:30:23 -0700

    hardcore https with the www in screenshot bang

--- app/models/post.rb
+++ app/models/post.rb
@@ -158,7 +158,7 @@ class Post < ActiveRecord::Base
 
   def screenshot!
     return false unless self.persisted?
-    frame_url = "#{AppConfig[:pod_url]}posts/#{self.guid}/frame"
+    frame_url = "https://www.makr.io/posts/#{self.guid}/frame"
     #maybe want to configure tmp directory,
     file = Screencap::Fetcher.new(frame_url).fetch(:div => '.canvas-frame:first', :output => Rails.root.join('tmp', "#{self.guid}.jpg"))
     self.screenshot.store!(file)
