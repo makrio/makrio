10365d743d0730751cf02e9386d7629ee56909c2
Dennis Collinson <dennis.collective@gmail.com>
2012-06-15 13:44:02 -0700

    fix job enqueuing, I hope

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -62,7 +62,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def update_profile_from_omniauth
     #current_user.update_profile(current_user.person.profile.from_omniauth_hash(user))
     fetch_photo = current_user.person.profile[:image_url].blank?
-    Resque.enqueue(Jobs::FetchProfilePhoto, current_user.id, service.id, user["image"]) if fetch_photo
+    Resque.enqueue(Jobs::FetchProfilePhoto, current_user.id, service.id, @user["image"]) if fetch_photo
   end
 
   def sign_in_or_register_user_from_facebook
