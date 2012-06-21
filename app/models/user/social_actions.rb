3b7ce2259d57dbe51023a748065e706f381d2f65
danielgrippi <danielgrippi@gmail.com>
2012-06-21 10:45:16 -0700

    Revert "like and love og actions for now until og.likes is approved for makr"
    This reverts commit 17d3ac3b38de080f17f81bab0c6e74b5c744f877.
    
    APPROVED.

--- app/models/user/social_actions.rb
+++ app/models/user/social_actions.rb
@@ -14,10 +14,7 @@ module User::SocialActions
     target || raise("liking needs a target")
     find_or_create_participation!(target)
     like = Like::Generator.new(self, target).create!(opts)
-
     open_graph_action('like', target)
-    open_graph_action('love', target) # to be removed as soon as og.likes is approved
-
     like
   end
 
