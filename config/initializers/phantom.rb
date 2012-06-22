f03ff541a503ac5db2980a2f955d9db15332ed45
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-22 10:38:39 -0700

    fix phantom output to not barf, and pipe the curl to dev null

--- config/initializers/phantom.rb
+++ config/initializers/phantom.rb
@@ -1,6 +1,6 @@
 if RUBY_PLATFORM.downcase.match(/darwin/)
   PHANTOM_PATH = 'phantomjs'
 else
-  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz | tar zx)`
+  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz > /dev/null 2>&1 | tar zx)`
   PHANTOM_PATH = '/app/vendor/phantomjs/bin/phantomjs'
 end
