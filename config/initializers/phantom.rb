56f17b0fa6f6c600a409b2c7192e60e79babe0b0
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-27 16:56:25 -0700

    Revert "try updating phantom.  this is scary"
    This reverts commit a22133922f09a89dcddc53fad90df8f11f21b17e.

--- config/initializers/phantom.rb
+++ config/initializers/phantom.rb
@@ -1,8 +1,6 @@
 if RUBY_PLATFORM.downcase.match(/darwin/)
   PHANTOM_PATH = 'phantomjs'
 else
-  phantom15 = 'http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz'
-  phantom16 = 'http://phantomjs.googlecode.com/files/phantomjs-1.6.0-linux-x86_64-dynamic.tar.bz2'
-  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl #{phantom16} | tar zx)`
+  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz | tar zx)`
   PHANTOM_PATH = '/app/vendor/phantomjs/bin/phantomjs'
 end
