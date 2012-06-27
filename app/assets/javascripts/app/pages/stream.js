3d8f902c13e2134a49659ed924ec1708ff90a074
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-27 12:21:12 -0700

    update some bookmarklet text

--- app/assets/javascripts/app/pages/stream.js
+++ app/assets/javascripts/app/pages/stream.js
@@ -28,7 +28,7 @@ app.pages.Stream = app.views.Base.extend({
 
   postRenderTemplate : function() {
     this.$('.dropdown-toggle').dropdown()
-
+    this.$('.bookmarklet').tooltip({placement: 'bottom'});
     if(app.currentUser.get("getting_started")) {
       this.showGettingStarted()
     }
@@ -93,7 +93,7 @@ app.pages.Stream = app.views.Base.extend({
   },
 
   bookmarkletJS : function() {
-    return "javascript:void(function(){ if(window.location.host.match(/makr/)){alert('Your trying to use the bookmarklet on makr.io, but you can remix directly on the site!');return};\
+    return "javascript:void(function(){ if(window.location.host.match(/makr/)){alert('Drag the \"Remix\" button to your bookmarks bar to easily remix any photo while you browse the web!');return};\
     var head= document.getElementsByTagName('head')[0]; \
     var script= document.createElement('script'); \
     script.type= 'text/javascript'; \
