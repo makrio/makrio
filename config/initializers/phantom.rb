a22133922f09a89dcddc53fad90df8f11f21b17e
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-27 16:25:02 -0700

    try updating phantom.  this is scary

--- config/initializers/phantom.rb
+++ config/initializers/phantom.rb
@@ -1,6 +1,8 @@
 if RUBY_PLATFORM.downcase.match(/darwin/)
   PHANTOM_PATH = 'phantomjs'
 else
-  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz | tar zx)`
+  phantom15 = 'http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz'
+  phantom16 = 'http://phantomjs.googlecode.com/files/phantomjs-1.6.0-linux-x86_64-dynamic.tar.bz2'
+  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl #{phantom16} | tar zx)`
   PHANTOM_PATH = '/app/vendor/phantomjs/bin/phantomjs'
 end
