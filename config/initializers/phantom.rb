27a6dc4a325a9b5ac5c32da1ae48a050a57eed27
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-27 17:27:55 -0700

    grr phantom

--- config/initializers/phantom.rb
+++ config/initializers/phantom.rb
@@ -3,6 +3,6 @@ if RUBY_PLATFORM.downcase.match(/darwin/)
 else
   phantom15 = 'http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz'
   phantom16 = 'http://phantomjs.googlecode.com/files/phantomjs-1.6.0-linux-x86_64-dynamic.tar.bz2'
-  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl #{phantom16} | tar xvj)`
-  PHANTOM_PATH = '/app/vendor/phantomjs/bin/phantomjs'
+  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl #{phantom16} | tar xj)`
+  PHANTOM_PATH = '/app/vendor/phantomjs-1.6.0-linux-x86_64-dynamic/bin/phantomjs'
 end
