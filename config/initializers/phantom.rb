58d4747300a48d60939b244c0537c86f3637447e
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-27 17:06:46 -0700

    phantom has a new compression for the file

--- config/initializers/phantom.rb
+++ config/initializers/phantom.rb
@@ -3,6 +3,6 @@ if RUBY_PLATFORM.downcase.match(/darwin/)
 else
   phantom15 = 'http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz'
   phantom16 = 'http://phantomjs.googlecode.com/files/phantomjs-1.6.0-linux-x86_64-dynamic.tar.bz2'
-  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl #{phantom16} | tar zx)`
+  `[ -f #{Rails.root}/vendor/phantomjs/ChangeLog ] &&  echo "Phantom is Loaded" || (cd #{Rails.root}/vendor && curl #{phantom16} | tar xvj)`
   PHANTOM_PATH = '/app/vendor/phantomjs/bin/phantomjs'
 end
