26c6deb6b0e3157b73feaa63408028892c9cc202
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-06-28 22:02:59 -0700

    make bookmarklet work on page with no HEAD tag (ie like single jpg pages)

--- app/assets/javascripts/app/pages/stream.js
+++ app/assets/javascripts/app/pages/stream.js
@@ -107,6 +107,7 @@ app.pages.Stream = app.views.Base.extend({
 
   bookmarkletJS : function() {
     return "javascript:void(function(){ if(window.location.host.match(/makr/)){alert('Drag the \"Remix\" button to your bookmarks bar to easily remix any photo while you browse the web!');return};\
+    if(document.getElementsByTagName('head').length ==0){document.getElementsByTagName('html')[0].appendChild(document.createElement('head'))} \
     var head= document.getElementsByTagName('head')[0]; \
     var script= document.createElement('script'); \
     script.type= 'text/javascript'; \
