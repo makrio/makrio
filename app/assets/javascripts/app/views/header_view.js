94365851db0c2f8fe71b7bce23c612b5f40dbfbe
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-03 10:49:33 -0700

    fix for firefox bookmarklet.
    firefox does not have document.location.origin :(

--- app/assets/javascripts/app/views/header_view.js
+++ app/assets/javascripts/app/views/header_view.js
@@ -30,14 +30,15 @@ app.views.Header = app.views.Base.extend({
   },
 
   bookmarkletJS : function() {
+    var location = document.location.protocol +"//"+ document.location.hostname
     return "javascript:void(function(){ if(window.location.host.match(/makr/)){alert('Drag the \"Remix\" button to your bookmarks bar to easily remix any photo while you browse the web!');return};\
     if(document.getElementsByTagName('head').length ==0){document.getElementsByTagName('html')[0].appendChild(document.createElement('head'))} \
     var head= document.getElementsByTagName('head')[0]; \
     var script= document.createElement('script'); \
     script.type= 'text/javascript'; \
-    script.src= '" + document.location.origin +  "/bookmarklet.js'; \
+    script.src= '" + location +  "/bookmarklet.js'; \
     script.id= 'makrio-bm-script'; \
-    script.setAttribute('data-origin','" + document.location.origin + "'); \
+    script.setAttribute('data-origin','" + location + "'); \
     head.appendChild(script);}());";
   },
 
