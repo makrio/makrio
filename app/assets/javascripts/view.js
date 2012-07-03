0b2219add62ea7b7b63aef6c4247fcad2a0ffec6
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-02 18:17:07 -0700

    fix the facebox image locations

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -13,8 +13,8 @@ var View = {
     $(this.avatars.selector).error(this.avatars.fallback);
 
     /* facebox */
-    $.facebox.settings.closeImage = '/assets/images/facebox/closelabel.png';
-    $.facebox.settings.loadingImage = '/assets/images/facebox/loading.gif';
+    $.facebox.settings.closeImage = '/assets/facebox/closelabel.png';
+    $.facebox.settings.loadingImage = '/assets/facebox/loading.gif';
     $.facebox.settings.opacity = 0.75;
 
     $('a[rel*=facebox]').facebox();
