0773f19dcd2c52cb6ad9eef7d059253b3b9a1990
danielgrippi <danielgrippi@gmail.com>
2012-07-03 13:30:48 -0700

    upgrade mixpanel [ci skip]

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -7,8 +7,8 @@ module AnalyticsHelper
     include_analytics "mixpanel" do
       javascript_tag do
         <<-JS.html_safe
-          (function(d,c){var a,b,g,e;a=d.createElement('script');a.type='text/javascript';a.async=!0;a.src=('https:'===d.location.protocol?'https:':'http:')+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';b=d.getElementsByTagName('script')[0];b.parentNode.insertBefore(a,b);c._i=[];c.init=function(a,d,f){var b=c;'undefined'!==typeof f?b=c[f]=[]:f='mixpanel';g='disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config'.split(' ');
-          for(e=0;e<g.length;e++)(function(a){b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,0)))}})(g[e]);c._i.push([a,d,f])};window.mixpanel=c})(document,[]);
+          (function(c,a){var b,d,h,e;b=c.createElement("script");b.type="text/javascript";b.async=!0;b.src=("https:"===c.location.protocol?"https:":"http:")+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';d=c.getElementsByTagName("script")[0];d.parentNode.insertBefore(b,d);a._i=[];a.init=function(b,c,f){function d(a,b){var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(Array.prototype.slice.call(arguments,0)))}}var g=a;"undefined"!==typeof f?g=
+          a[f]=[]:f="mixpanel";g.people=g.people||[];h="disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config people.set people.increment".split(" ");for(e=0;e<h.length;e++)d(g,h[e]);a._i.push([b,c,f])};a.__SV=1.1;window.mixpanel=a})(document,window.mixpanel||[]);
           mixpanel.init("#{AppConfig[:mixpanel_uid]}");
         JS
       end
@@ -20,7 +20,10 @@ module AnalyticsHelper
     include_analytics "mixpanel" do
       javascript_tag do
         <<-JS.html_safe
-          mixpanel.name_tag("#{current_user.username}");
+          mixpanel.identify("#{current_user.username}");
+          mixpanel.people.set({
+            "$created": "#{current_user.created_at}"
+          })
         JS
       end
     end
