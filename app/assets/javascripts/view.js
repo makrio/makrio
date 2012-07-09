1aa2d6e797371238107bd7c820566a23633ed8ee
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-09 16:18:55 -0700

    clean up facebox images

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -13,10 +13,6 @@ var View = {
     $(this.avatars.selector).error(this.avatars.fallback);
 
     /* facebox */
-    $.facebox.settings.closeImage = '/assets/facebox/closelabel.png';
-    $.facebox.settings.loadingImage = '/assets/facebox/loading.gif';
-    $.facebox.settings.opacity = 0.9;
-
     $('a[rel*=facebox]').facebox();
 
   },
