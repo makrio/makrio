6ea3affdd5b0ae6e95a58a9fff13560fa3f29180
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-14 03:13:29 -0700

    kill lightbox

--- app/assets/javascripts/app/views/photos_view.js
+++ app/assets/javascripts/app/views/photos_view.js
@@ -6,18 +6,9 @@ app.views.Photos = app.views.InfScroll.extend({
     // viable for extraction
     this.stream.fetch();
 
-    this.setupLightbox()
     this.setupInfiniteScroll()
   },
 
   postClass : app.views.Photo,
 
-  setupLightbox : function(){
-    this.lightbox = Diaspora.BaseWidget.instantiate("Lightbox");
-    this.lightbox.set({
-      imageParent: '#main_stream',
-      imageSelector: 'img.photo'
-    });
-    $(this.el).delegate("a.photo-link", "click", this.lightbox.lightboxImageClicked);
-  }
 });
--- app/assets/javascripts/app/views/stream_view.js
+++ app/assets/javascripts/app/views/stream_view.js
@@ -6,16 +6,11 @@ app.views.Stream = app.views.InfScroll.extend({
     this.postViews = []
 
     this.setupNSFW()
-    this.setupLightbox()
     this.setupInfiniteScroll()
   },
 
   postClass : app.views.StreamPost,
 
-  setupLightbox : function(){
-    this.lightbox = Diaspora.BaseWidget.instantiate("Lightbox");
-    this.$el.delegate("a.stream-photo-link", "click", this.lightbox.lightboxImageClicked);
-  },
 
   setupNSFW : function(){
     app.currentUser.bind("nsfwChanged", reRenderPostViews, this)
