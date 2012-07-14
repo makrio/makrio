e668d5fe7117ab35b1f681561493367c5fac112f
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-14 11:53:48 -0700

    more blackklist for username

--- config/environment.rb
+++ config/environment.rb
@@ -17,8 +17,9 @@ Haml::Template.options[:format] = :html5
 Haml::Template.options[:escape_html] = true
 
 # Blacklist of usernames
-USERNAME_BLACKLIST = ['admin', 'administrator', 'hostmaster', 'info', 'postmaster', 'root', 'ssladmin', 
-  'ssladministrator', 'sslwebmaster', 'sysadmin', 'webmaster', 'support', 'contact', 'example_user1dsioaioedfhgoiesajdigtoearogjaidofgjo']
+USERNAME_BLACKLIST = ['admin', 'admin_panel', 'admin-panel', 'stream', 'staff_picks', 'likes', 'conversations', 'administrator', 'hostmaster', 'info', 'postmaster', 'root', 'ssladmin', 
+  'ssladministrator', 'sslwebmaster', 'sysadmin', 'webmaster', 'support', 'contact', 'settings', 'account', 'help', 'signout', 'sign_out', 'sign_in', 'logout', 'login','posts', 
+'threads', 'notifications', 'messages', 'resque-jobs', 'profile']
 
 # Initialize the rails application
 Diaspora::Application.initialize!
