ec5b2c5f1710286f6f07846e1b299617419d997b
danielgrippi <danielgrippi@gmail.com>
2012-07-14 18:28:05 -0700

    don't use backbone navigation in conversationFrame for now.

--- app/assets/javascripts/app/views/conversation_frame.js
+++ app/assets/javascripts/app/views/conversation_frame.js
@@ -2,10 +2,6 @@ app.views.Post.ConversationFrame = app.views.Base.extend({
 
   className : 'row cnv-row',
 
-  events : {
-    "click .frame" : "goToCollection"
-  },
-
   templateName : 'conversation-frame',
 
   initialize : function() {
@@ -17,10 +13,5 @@ app.views.Post.ConversationFrame = app.views.Base.extend({
     return _.extend(this.defaultPresenter(), {
       conversation : this.conversation.attributes
     })
-  },
-
-  goToCollection : function(evt) {
-    evt && evt.preventDefault()
-    app.router.setLocation("/conversations/" + this.model.get('conversation_id'))
   }
 });
