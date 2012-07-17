a2201181d523dedcb042076801e359d9c3ea41b9
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-17 13:09:06 -0700

    refactor comments to be a subview of the interaction pane so I can re-use it for the modal framer

--- app/assets/javascripts/app/views/post-viewer/reactions.js
+++ app/assets/javascripts/app/views/post-viewer/reactions.js
@@ -6,15 +6,14 @@ app.views.PostViewerReactions = app.views.Base.extend({
 
   tooltipSelector : ".avatar",
 
-  initialize : function() {
-    this.model.on('change', this.render, this);
-    this.model.comments.on("add", this.appendComment, this)
+  subviews : {
+    '.posts-comments-container' : 'commentView'
   },
 
-  unbind : function(){
-    this.model.off("change", this.render, this)
-    this.model.comments.off("add", this.appendComment, this)
+  initialize : function(){
+    this.commentView = new app.views.PostViewerComments({model : this.model})
   },
+
 
   presenter : function(){
     return {
@@ -23,6 +22,21 @@ app.views.PostViewerReactions = app.views.Base.extend({
       comments : this.model.comments.toJSON(),
       remixes : this.model.remixes.toJSON()
     }
+  },
+
+
+});
+
+app.views.PostViewerComments = app.views.Base.extend({
+  templateName : 'post-viewer-comments',
+  initialize : function() {
+    this.model.on('change', this.render, this);
+    this.model.comments.on("add", this.appendComment, this)
+  },
+
+  unbind : function(){
+    this.model.off("change", this.render, this)
+    this.model.comments.off("add", this.appendComment, this)
   },
 
   postRenderTemplate : function() {
@@ -38,10 +52,10 @@ app.views.PostViewerReactions = app.views.Base.extend({
   appendComment: function(comment) {
     // Set the post as the comment's parent, so we can check on post ownership in the Comment view.
     // model was post on old view, is interactions on new view
-
     var parent = this.model.get("post_type") ? this.model.toJSON : this.model.post.toJSON()
-    comment.set({parent : parent})
 
+    comment.set({parent : parent})
+    console.log(this.$("#post-comments"))
     this.$("#post-comments").append(new app.views.Comment({
       model: comment,
       className : "post-comment media",
