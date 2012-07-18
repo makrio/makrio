1dad92d1ed851e5974bae12b22d856f8553a0ac0
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-17 17:55:31 -0700

    sad panda.  fix adding to staff picks

--- app/assets/javascripts/app/views/post/stream_frame.js
+++ app/assets/javascripts/app/views/post/stream_frame.js
@@ -18,6 +18,9 @@ app.views.Post.StreamFrame = app.views.Base.extend({
   initialize : function(options) {
     this.stream = options.stream
     this.smallFrameView = new app.views.Post.SmallFrame({ model : this.model })
+    
+    //:( we are double instanticating this via the parent and a getter method where we do not memoize
+    this.feedbackView = new app.views.StreamFeedbackActions({ model: this.model })
   },
 
   presenter : function() {
