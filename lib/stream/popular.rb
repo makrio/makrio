af3d1c3f3edd7fe9b2fcc8619bbc1b0cfe8a71b2
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-27 14:47:57 -0700

    filter out unfettered on popular stream.

--- lib/stream/popular.rb
+++ lib/stream/popular.rb
@@ -19,7 +19,7 @@ class Stream::Popular < Stream::Base
     # @posts ||= Post.order("(exp(posts.likes_count) + posts.comments_count*2 - (exp(extract(hour from age(created_at))/24))) DESC")
 
     # THE REDDIT ALGOOO <3333333333333 kent
-    @posts ||= Post.order("(ln( 1 + posts.likes_count) +  (EXTRACT(EPOCH FROM created_at) - 1327654606)/9000) desc")
+    @posts ||= Post.featured_and_by_author(user.try(:person)).order("(ln( 1 + posts.likes_count) +  (EXTRACT(EPOCH FROM created_at) - 1327654606)/9000) desc")
   end
 
   def contacts_title
