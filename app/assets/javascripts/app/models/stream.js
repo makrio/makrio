32b460d3c9952c74789ab09a83bb0ef1b5b6389c
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-08-05 15:17:22 -0700

    since polling is wip, making this false to have same behavior

--- app/assets/javascripts/app/models/stream.js
+++ app/assets/javascripts/app/models/stream.js
@@ -40,7 +40,7 @@ app.models.Stream = Backbone.Collection.extend({
   initialize : function(models, options){
     var collectionClass = options && options.collection || app.collections.Posts;
     var collectionOpts = options && options.collectionOptions || this.collectionOptions()
-    this.poll  = options && options.poller || true
+    this.poll  = options && options.poller || false
     this.items = new collectionClass([], collectionOpts);
     this.on("loadNew", this.addPollerPosts, this)
   },
