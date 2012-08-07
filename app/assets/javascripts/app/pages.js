530203a9ee90907d6f306a7c93c260aac320110c
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-08-06 19:04:23 -0700

    show the sign up banner on most of the pages

--- app/assets/javascripts/app/pages.js
+++ app/assets/javascripts/app/pages.js
@@ -1,8 +1,21 @@
+app.views.SignUpBannerView = app.views.Base.extend({
+  templateName : 'sign-up-banner'
+});
+
+
 app.pages.Base = app.views.Base.extend({
   renderBaseViews : function() {
     if($("#header").length != 0) { return }
       
     this.headerView = new app.views.Header()
     $("body").prepend(this.headerView.render().el)
+
+    if(!app.currentUser.authenticated()){
+      var signUp = this.$('#sign_up_strip') // I am faking this, sorry
+      if(signUp.length ==1){
+        this.signUpBannerView = new app.views.SignUpBannerView({model: this.model})
+        $('#header').after(this.signUpBannerView.render().el)
+      }
+    }
   }
 });
