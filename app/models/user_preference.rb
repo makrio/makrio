f65dee08bfe1f7918148698ddbdee25869694670
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-08-20 20:36:36 -0700

    need setting for followed

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -10,7 +10,8 @@ class UserPreference < ActiveRecord::Base
    "started_sharing",
    "also_commented",
    "liked",
-    "remixed"]
+    "remixed",
+    'followed']
 
 
   def must_be_valid_email_type
