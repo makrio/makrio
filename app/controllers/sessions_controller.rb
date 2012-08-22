d475148d908ea4da14d2aa6241a98547a423d615
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-08-22 15:23:50 -0700

    fix sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < Devise::SessionsController
   def create
     fb_data = session["devise.facebook_data"]
     if fb_data
-      service = Services::Facebook.find_by_uid_and_access_secret!(fb_data.uid, fb_data.credentials.secret)
+      service = Services::Facebook.find_by_uid_and_access_secret!(fb_data['uid'], fb_data['credentials']['secret'])
     end
 
     super
