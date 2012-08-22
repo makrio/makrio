39b92b0c2f8d3fb04df0328526b0e55fdfab4e50
Daniel Grippi <danielgrippi@gmail.com>
2012-08-22 01:28:20 -0700

    don't show an add post button on the popular page.

--- app/assets/javascripts/app/views/infinite_stream_view.js
+++ app/assets/javascripts/app/views/infinite_stream_view.js
@@ -100,7 +100,7 @@ app.views.InfScroll = app.views.Base.extend({
     }, this))
 
     function showAddButton() {
-      var rejectedPaths = ['staff_picks','latest','likes','conversations','feed'] 
+      var rejectedPaths = ['front_page','staff_picks','latest','likes','conversations','feed'] 
       return (_.reject(rejectedPaths, function(path){return window.location.pathname.search(path) == -1}).length == 0) && !this.onProfilePage 
     }
   },
